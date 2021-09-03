be143349a519171b30b50e20c5b3792febc78fda
hayjo <hay.hyunjo@gmail.com>
2021-09-03 23:05:22 +0900

    Initialize CRA

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
+
+function App() {
+  return (
+    <AppWrapper>
+      <header />
+      <div>Render</div>
+    </AppWrapper>
+  );
+}
+
+export default App;
---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { firebaseReducer } from 'react-redux-firebase';
+import logger from 'redux-logger';
+
+export const store = configureStore({
+  reducer: {
+    firebase: firebaseReducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
