6cd2ea0ebe0562bd726ccc209ffaa301f53cf206
Hyun Jo <hay.hyunjo@gmail.com>
2021-09-05 01:18:24 +0900

    Add: login/logout feature

---
+++ src/features/userSlice.js
@@ -0,0 +1,45 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import api from "../utils/api";
+import { storeTokens, removeTokens } from "../utils/tokens";
+
+const initialState = {
+  hasLoggedIn: false
+};
+
+const login = createAsyncThunk("user/login",
+  async function (idToken) {
+    try {
+      const { accessToken, refreshToken } = await api.login(idToken);
+
+      storeTokens({ accessToken, refreshToken });
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  });
+
+const logout = createAsyncThunk("user/logout", removeTokens);
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  extraReducers: {
+    [login.fulfilled]: (state) => {
+      state.hasLoggedIn = true;
+    },
+    [login.pending]: (state) => {
+      state.hasLoggedIn = false;
+    },
+    [login.rejected]: (state) => {
+      state.hasLoggedIn = false;
+    },
+    [logout.fulfilled]: (state) => {
+      state.hasLoggedIn = false;
+    },
+    [logout.rejected]: (state) => {
+      state.hasLoggedIn = true;
+    },
+  }
+});
+
+export { login, logout };
+export default userSlice.reducer;
---
+++ src/utils/tokens.js
@@ -0,0 +1,26 @@
+import Cookie from "js-cookie";
+import api from "./api";
+
+const REFRESH_TOKEN = "refreshToken";
+const ACCESS_TOKEN = "accessToken";
+
+async function updateAccessToken() {
+  const refreshToken = Cookie.get(REFRESH_TOKEN);
+  const accessToken = await api.refresh(refreshToken);
+
+  localStorage.setItem(ACCESS_TOKEN, accessToken);
+}
+
+function storeTokens({ accessToken, refreshToken }) {
+  localStorage.setItem(ACCESS_TOKEN, accessToken);
+  Cookie.set(REFRESH_TOKEN, refreshToken);
+}
+
+function removeTokens() {
+  localStorage.removeItem(ACCESS_TOKEN);
+  Cookie.remove(REFRESH_TOKEN);
+}
+
+export {
+  updateAccessToken, storeTokens, removeTokens
+};
