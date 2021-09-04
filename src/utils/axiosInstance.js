7206cfcbee1ced5e27d3d149611737f419db817d
Hyun Jo <hay.hyunjo@gmail.com>
2021-09-05 01:21:00 +0900

    Add: axiosInstance with token header

---
+++ src/utils/axiosInstance.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { ERROR } from "../constants/messages";
+import STATUS_CODE from "../constants/statusCodes";
+
+const { INTERNAL_SERVER_ERROR } = STATUS_CODE;
+
+function setAccessToken(config) {
+  const accessToken = localStorage.getItem("accessToken");
+
+  if (!accessToken) {
+    throw new axios.Cancel(ERROR.ACCESS_TOKEN_NOT_EXIST);
+  }
+
+  config.headers.token = accessToken;
+
+  return config;
+}
+
+function handleRequestError(err) {
+  return Promise.reject(err);
+}
+
+function parseResponseData(response) {
+  return response.data;
+}
+
+function handleResponseError(err) {
+  console.log(err.response, INTERNAL_SERVER_ERROR);
+  if (err.response?.status < INTERNAL_SERVER_ERROR) {
+    return err.response;
+  }
+
+  throw new Error(ERROR.INTERNAL_SERVER_ERROR);
+}
+
+const instance = axios.create();
+
+instance.interceptors.request.use(setAccessToken, handleRequestError);
+instance.interceptors.response.use(parseResponseData, handleResponseError);
+
+export default instance;
