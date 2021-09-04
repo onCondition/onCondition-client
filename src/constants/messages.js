a3e75861682fc82181e6cd03305ed1c81a3b0766
Hyun Jo <hay.hyunjo@gmail.com>
2021-09-05 01:20:23 +0900

    Add: constants/errors, messages, statusCodes

---
+++ src/constants/messages.js
@@ -0,0 +1,8 @@
+const ERROR = {
+  LOGIN_FAIL: "로그인에 실패하였습니다.",
+  TOKEN_REFRESH_FAIL: "토큰 갱신에 실패하였습니다.",
+  INTERNAL_SERVER_ERROR: "현재 서버 이용이 불가능합니다.",
+  ACCESS_TOKEN_NOT_EXIST: "로그인 정보를 확인할 수 없습니다."
+};
+
+export { ERROR };
