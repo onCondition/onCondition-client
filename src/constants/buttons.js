b5f4b75e2b34384787a682c77c29a250870ee28e
Hyun Jo <hay.hyunjo@gmail.com>
2021-09-06 02:03:00 +0900

    Add: src/constants/buttons, colors, webFontUrl

---
+++ src/constants/buttons.js
@@ -0,0 +1,9 @@
+const COMMENT = "COMMENT";
+const EDIT = "EDIT";
+const DELETE = "DELETE";
+
+export {
+  COMMENT,
+  EDIT,
+  DELETE,
+};
---
+++ src/constants/colors.js
@@ -0,0 +1,9 @@
+const COLORS = {
+  MAIN_PINK: "#F97171",
+  LIGHT_PINK: "#FB9C9C",
+  DARK_GREY: "#B0B0B0",
+  LIGHT_GREY: "#F5F6F6",
+  WHITE: "#FFFFFF",
+};
+
+export default COLORS;
