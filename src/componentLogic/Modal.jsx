376ebbe147b4eda31d4646a0802ea0061f8664c1
Soodev20 <soodev20@gmail.com>
2021-09-05 23:13:25 +0900

    Chore: Delete console log

--- src/componentLogic/Modal.jsx
+++ src/componentLogic/Modal.jsx
@@ -5,12 +5,10 @@ function Modal() {
   const [modal, setModal] = useState(true);
 
   const onConfirm = () => {
-    console.log("확인");
     setModal(false);
   };
 
   const onCancel = () => {
-    console.log("취소");
     setModal(false);
   };
 
