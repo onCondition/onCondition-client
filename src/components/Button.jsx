25f0d87bbd54173acd53f9880c74885d62c99581
Hyun Jo <hay.hyunjo@gmail.com>
2021-09-06 02:04:43 +0900

    Modify: Button style, add color, backgroundColor props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,20 +1,52 @@
 import React from "react";
 import PropTypes from "prop-types";
+import FONTS from "../constants/webFontUrl";
+import styled from "styled-components";
+import COLORS from "../constants/colors";
 
-function Button({ onClick, text }) {
+const SButton = styled.button`
+  padding: 0.4rem 0.7rem;
+  margin: 4px;
+  background-color: ${(props) => props.backgroundColor};
+  border: none;
+  border-radius: 30px;
+  color: ${(props) => props.color};
+
+  :hover {
+    cursor: pointer;
+  }
+
+  @font-face {
+    font-family: "Carrois Gothic SC", sans-serif;
+    src: url(${FONTS.CARROIS_GOTHIC_SC});
+  }
+`;
+
+function Button({
+  onClick, text, color, backgroundColor
+}) {
   return (
-    <button
+    <SButton
       type="button"
       onClick={onClick}
+      color={color}
+      backgroundColor={backgroundColor}
     >
       {text}
-    </button>
+    </SButton>
   );
 }
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
   text: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  backgroundColor: PropTypes.string,
+};
+
+Button.defaultProps = {
+  color: COLORS.WHITE,
+  backgroundColor: COLORS.LIGHT_PINK,
 };
 
 export default Button;
