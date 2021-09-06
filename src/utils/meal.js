a1d911144c8fddfbe4f2eb182cefc2ebf2510b7f
hayjo <hay.hyunjo@gmail.com>
2021-09-06 23:21:15 +0900

    Add getMeals

--- src/utils/meal.js
+++ src/utils/meal.js
@@ -1,7 +1,13 @@
 import axios from "./axiosInstance";
 
+async function getMeals() {
+  const res = await axios.get("/api/meal");
+  console.log(res);
+  return res.data;
+}
+
 async function postMeal(data) {
   await axios.post("/api/meal", data);
 }
 
-export { postMeal };
+export { getMeals, postMeal };
