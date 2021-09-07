36209f242fc5bf223a7c146025fa855c91a0a678
Soodev20 <soodev20@gmail.com>
2021-09-07 18:43:56 +0900

    Add: More constants variables

--- src/constants/numbers.js
+++ src/constants/numbers.js
@@ -1,10 +1,22 @@
-const SIZE = {
-  SMALL: 28,
+const TEXT_SIZE = {
   DIVIDE_OCTUPLE: 8,
+  SMALL: 28,
   MEDIUM: 36,
   LARGE: 44,
   X_LARGE: 52,
   TITLE: 100,
+};
+
+const GAP_SIZE = {
+  FIVE: 5,
+  TEN: 10,
+  TWENTY: 20,
+  FORTY: 40,
+};
+
+const SIZE = {
+  TEXT_SIZE,
+  GAP_SIZE,
 };
 
 export default SIZE;
