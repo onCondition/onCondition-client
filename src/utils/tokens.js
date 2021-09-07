35949a1e1ed2ed85f2bad5ceaaa81738aa40e499
Hyun Jo <hay.hyunjo@gmail.com>
2021-09-07 15:44:14 +0900

    Add: googleToken utils

--- src/utils/tokens.js
+++ src/utils/tokens.js
@@ -1,3 +1,4 @@
+import qs from "qs";
 import Cookie from "js-cookie";
 import { postRefresh } from "./auth";
 
@@ -21,6 +22,46 @@ function removeTokens() {
   Cookie.remove(REFRESH_TOKEN);
 }
 
+function getGoogleTokenRequestUrl() {
+  const SCOPE = [
+    "https://www.googleapis.com/auth/fitness.activity.read",
+    "https://www.googleapis.com/auth/fitness.sleep.read",
+  ].join(" ");
+  const queryString = qs.stringify({
+    client_id: process.env.REACT_APP_CLIENT_ID,
+    redirect_uri: process.env.REACT_APP_USER_CONSENT_REDIRECT_URI,
+    response_type: "token",
+    scope: SCOPE,
+  });
+
+  return process.env.REACT_APP_AUTH_URI + "?" + queryString;
+}
+
+function parseGoogleToken(queryString) {
+  const tokenInfo = qs.parse(queryString);
+  const {
+    access_token: accessToken,
+    token_type: tokenType,
+    expires_in: expiresIn,
+    scope,
+  } = tokenInfo;
+
+  if (!accessToken) {
+    return null;
+  }
+
+  return {
+    accessToken,
+    tokenType,
+    expiresIn,
+    scope,
+  };
+}
+
 export {
-  updateAccessToken, storeTokens, removeTokens,
+  updateAccessToken,
+  storeTokens,
+  removeTokens,
+  getGoogleTokenRequestUrl,
+  parseGoogleToken,
 };
