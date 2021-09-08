f154d4c8e9076f0911399002bd132de7b80017ee
hayjo <60309558+hayjo@users.noreply.github.com>
2021-09-08 20:23:32 +0900

    Chore: remove lint comment

--- src/constants/webFontUrl.js
+++ src/constants/webFontUrl.js
@@ -1,5 +1,3 @@
-/* eslint-disable max-len */
-
 const FONTS = {
   NANUM_GOTHIC: "https://fonts.googleapis.com/css2?family=Nanum+Gothic",
   CARROIS_GOTHIC_SC: "https://fonts.googleapis.com/css2?family=Carrois+Gothic+SC",
