e579ef4dee53bad504ac0c18a80c63da6310d146
Hyun Jo <hay.hyunjo@gmail.com>
2021-09-08 17:54:29 +0900

    Modify: Button values as theme values

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import theme from "../theme";
 
 const ButtonSt = styled.button`
   display: relative;
   border-radius: 10px;
   border: none;
-  background-color: ${(props) => props.backgroundColor};
-  box-shadow: 5px 5px 10px black;
-  color: white;
-  cursor: pointer;
-  font-size: large;
-  width: ${(props) => props.width};
-  height: ${(props) => props.height};
+  background-color: ${(prop) => prop.backgroundColor};
+  box-shadow: 5px 5px 10px ${({ theme }) => theme.greyScaleColors.darkGrey};
+  color: ${({ theme }) => theme.innerColors.button};
+  font-size: ${({ theme }) => theme.fontSizes.darkGrey};
+  width: ${(prop) => prop.width};
+  height: ${(prop) => prop.height};
   margin: 0 auto;
   margin-top: 15px;
 `;
@@ -45,7 +45,7 @@ Button.propTypes = {
 };
 
 Button.defaultProps = {
-  backgroundColor: "#FFFFFF",
+  backgroundColor: theme.pinkColors.lightPink,
   height: "50px",
   width: "100px",
 };
