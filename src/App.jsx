1e400529658dcc0e29fd7a31e5a0ab8e3643cc31
hayjo <60309558+hayjo@users.noreply.github.com>
2021-09-08 20:21:55 +0900

    Fix: add ThemeProvider, GlobalStyle

--- src/App.jsx
+++ src/App.jsx
@@ -39,31 +39,34 @@ function App() {
   }, []);
 
   return (
-    <AppWrapper>
-      {isLoaded ? (
-        <>
-          <header>
-            <Logout onLogout={handleLogout} />
-          </header>
-          <Switch>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <PrivateRoute exact path="/myCondition">
-              <p>my condition</p>
-            </PrivateRoute>
-            <PrivateRoute exact path="/meal">
-              <Meal />
-            </PrivateRoute>
-            <PrivateRoute exact path="/meal/:id">
-              <MealDetail />
-            </PrivateRoute>
-          </Switch>
-        </>
-      ) : (
-        <p>waiting...</p>
-      )}
-    </AppWrapper>
+    <ThemeProvider theme={theme}>
+      <AppWrapper>
+        {isLoaded ? (
+          <>
+            <header>
+              <Logout onLogout={handleLogout} />
+            </header>
+            <Switch>
+              <Route path="/login">
+                <Login />
+              </Route>
+              <PrivateRoute exact path="/myCondition">
+                <p>my condition</p>
+              </PrivateRoute>
+              <PrivateRoute exact path="/meal">
+                <Meal />
+              </PrivateRoute>
+              <PrivateRoute exact path="/meal/:id">
+                <MealDetail />
+              </PrivateRoute>
+            </Switch>
+          </>
+        ) : (
+          <p>waiting...</p>
+        )}
+      </AppWrapper>
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
 
