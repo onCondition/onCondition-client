b22b308496ee3fb7770ab6ac5118fd89fac1f370
Hyun Jo <hay.hyunjo@gmail.com>
2021-09-12 00:38:23 +0900

    Refactor: merge category apis

---
+++ src/api/category.js
@@ -0,0 +1,69 @@
+import axios from "./axiosInstance";
+
+const BASE = "/api";
+
+function joinUrl(...args) {
+  return args.join("/");
+}
+
+async function get(category, id, page) {
+  const res = await axios.get(joinUrl(BASE, id, category), {
+    headers: {
+      page,
+    },
+  });
+
+  if (res) {
+    const { data, prevPage, nextPage } = res;
+
+    return { data, prevPage, nextPage };
+  }
+}
+
+async function post(category, id, data) {
+  const res = await axios.post(joinUrl(BASE, id, category), data);
+
+  if (res) {
+    return res.data;
+  }
+}
+
+async function getById(category, ratingId) {
+  const res = await axios.get(joinUrl(BASE, category, ratingId));
+
+  if (res) {
+    return res.data;
+  }
+}
+
+async function editById(category, id, ratingId, data) {
+  const res = await axios.patch(joinUrl(BASE, id, category, ratingId), data);
+
+  return res;
+}
+
+async function deleteById(category, id, ratingId) {
+  const res = await axios.delete(joinUrl(BASE, id, category, ratingId));
+
+  return res;
+}
+
+function generateApiInstance(category) {
+  return {
+    get: get.bind(null, category),
+    post: post.bind(null, category),
+    getById: getById.bind(null, category),
+    editById: editById.bind(null, category),
+    deleteById: deleteById.bind(null, category),
+  };
+}
+
+const api = {
+  meal: generateApiInstance("meal"),
+  activity: generateApiInstance("activity"),
+  sleep: generateApiInstance("sleep"),
+  customAlbum: generateApiInstance("customAlbum"),
+  customGrid: generateApiInstance("customGrid"),
+};
+
+export default api;
