369f024e88a273bd71956ab072aa079cb49ad65c
Hyun Jo <hay.hyunjo@gmail.com>
2021-09-16 21:55:51 +0900

    Fix: rollback login action for setting initial userInfo

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import styled from "styled-components";
 
 import Loading from "../components/Loading";
 import firebase from "../config/firebase";
-import { postLogin } from "../api/auth";
-import { storeUserInfos } from "../helpers/userInfo";
+import { login } from "../features/userSlice";
 import { ERROR } from "../constants/messages";
 import USER_INFO_SCOPE from "../constants/userInfoScope";
 
@@ -20,6 +19,7 @@ function Login() {
   const [status, setStatus] = useState("");
   const user = useSelector((state) => state.user);
   const history = useHistory();
+  const dispatch = useDispatch();
 
   function loginWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
@@ -48,10 +48,9 @@ function Login() {
 
         const idToken = await user.getIdToken(true);
         const googleAccessToken = credential.accessToken;
-        const { accessToken, refreshToken } = await postLogin(idToken);
 
-        storeUserInfos({ accessToken, refreshToken, googleAccessToken });
-      } catch {
+        dispatch(login({ idToken, googleAccessToken }));
+      } catch (err) {
         setStatus(ERROR.LOGIN_FAIL);
       }
     }
--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,10 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { postLogin, getUserInfos } from "../api/auth";
+import { storeUserInfos, removeUserInfos } from "../helpers/userInfo";
 
 const initialState = {
   id: "",
   customCategories: [],
   lastAccessDate: "",
 };
+
+const login = createAsyncThunk("user/login",
+  async function ({ idToken, googleAccessToken }) {
+    try {
+      const { accessToken, refreshToken } = await postLogin(idToken);
+
+      storeUserInfos({ accessToken, refreshToken, googleAccessToken });
+
+      const { userId, customCategories, lastAccessDate } = await getUserInfos();
+
+      return {
+        userId, customCategories, lastAccessDate,
+      };
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  });
+
+const logout = createAsyncThunk("user/logout", removeUserInfos);
 
 const userSlice = createSlice({
   name: "user",
@@ -27,7 +48,25 @@ const userSlice = createSlice({
       state.customCategories = payload;
     },
   },
+  extraReducers: {
+    [login.fulfilled]: (state, { payload }) => {
+      state.id = payload.userId;
+      state.customCategories = payload.customCategories;
+      state.lastAccessDate = payload.lastAccessDate;
+    },
+    [login.rejected]: (state) => {
+      state.id = null;
+      state.customCategories = [];
+      state.lastAccessDate = null;
+    },
+    [logout.fulfilled]: (state) => {
+      state.id = null;
+      state.customCategories = [];
+      state.lastAccessDate = null;
+    },
+  },
 });
 
 export const { setUserInfos, setCustomCategories } = userSlice.actions;
+export { login, logout };
 export default userSlice.reducer;
