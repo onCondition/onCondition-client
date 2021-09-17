b5c3330be265cc3dd4a381f72eeba78ff3125f33
ParkJeongseon <wjdtjs529@gmail.com>
2021-09-18 01:24:26 +0900

    Modify: resize sleep graph

--- src/pages/Sleep.jsx
+++ src/pages/Sleep.jsx
@@ -1,9 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import styled from "styled-components";
 
 import BarGraph from "../components/graphs/BarGraph";
 import { PrevButton, NextButton } from "../components/PageButton";
 import getApi from "../api/category";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .graph {
+    width: calc(100% - 80px);
+  }
+`;
 
 function Sleep() {
   const [sleepData, setSleepData] = useState(null);
@@ -35,14 +46,18 @@ function Sleep() {
   return (
     <div>
       <h1>수면</h1>
-      <div>
-        <PrevButton />
-        <div>{periods}</div>
-        <NextButton />
-      </div>
-      <BarGraph
-        IncomingData={sleepData}
-      />
+      <Container>
+        <div className="navigator">
+          <PrevButton />
+          <div>{periods}</div>
+          <NextButton />
+        </div>
+        <div className="graph">
+          <BarGraph
+            IncomingData={sleepData}
+          />
+        </div>
+      </Container>
     </div>
   );
 }
